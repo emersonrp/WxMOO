937b027b2fff2ee79f0c1c1d6695a5ce939ad240
Russell Pickett <emerson@craigslist.org>
2013-08-11 01:38:39 -0700

    Config file
    Now we have a config .ini file, and can set fonts with it.
    Long way to go still, and this might still all get refactored or
    re-engineered.

--- WxMOO/Prefs.pm
+++ WxMOO/Prefs.pm
@@ -1,33 +1,61 @@
 package WxMOO::Prefs;
 use perl5i::2;
 use Wx qw( :font );
+use Config::IniFiles;
 
 use base 'Class::Singleton';
+
+# TODO - cross-platform config file locater
+my $FILENAME = "$ENV{'HOME'}/.wxmoorc";
 
 method _new_instance($class: %init) {
         my $self = bless {%init}, $class;
         # TODO:  $self = get_my_prefs_from_storage;
 
-# TODO this is hard-coded for now until we actually save prefs
-my $font= Wx::Font->new( 10, wxTELETYPE, wxNORMAL, wxNORMAL );
-$self->input_font($font);
-$self->output_font($font);
+        my $confFileExists = (-e $FILENAME);
+        my $initialState = $confFileExists ? $FILENAME : initialDefaults();
+
+        $self->{'cfg'} = Config::IniFiles->new(
+            -file       => $initialState,
+            -default    => 'default',
+            -nocase     => 1,
+            -allowempty => 1,
+        );
+        unless ($confFileExists) {
+            $self->{'cfg'}->WriteConfig($FILENAME) or carp "couldn't write";
+        }
 
         return $self;
 }
-# TODO - represent this internally as:
-# $prefs = { saved => { font => 'stan', love => 'like oxygen',... },
-#            current => { font => 'something else',...}
-# }
-# so we can know if the prefs are 'dirty' and need saving, and
-# also 'revert to saved'
 
-method input_font($new) {
-    $self->{'input_font'} = $new if $new;
-    $self->{'input_font'};
-}
+# INITIAL DEFAULTS
+{
+    method input_font($new) {
+        state $font //= Wx::Font->new($self->{'cfg'}->val('default','input_font'));
+         if ($new) {
+             $self->{'cfg'}->setval('default','input_font', $new);
+             $font->SetNativeFontInfo($new);
+         }
+        return $font;
+    }
 
-method output_font($new) {
-    $self->{'output_font'} = $new if $new;
-    $self->{'output_font'};
+    method output_font($new) {
+        state $font //= Wx::Font->new($self->{'cfg'}->val('default','output_font'));
+         if ($new) {
+             $self->{'cfg'}->setval('default','output_font', $new);
+             $font->SetNativeFontInfo($new);
+         }
+        return $font;
+    }
+
+    sub initialDefaults {
+        my $defaultFont = Wx::Font->new( 10, wxTELETYPE, wxNORMAL, wxNORMAL );
+        my $defaultFontString = $defaultFont->GetNativeFontInfo->ToString;
+
+        return \"
+[default]
+input_font=$defaultFontString
+output_font=$defaultFontString
+"
+    }
 }
